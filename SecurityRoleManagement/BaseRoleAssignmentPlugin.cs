f6dba84ba28d947965e140ba6371c3a03cb401ce
Blake Bradford <blake@403consulting.com>
2025-03-02 08:29:39 -0500

    Add BaseRoleAssignmentPlugin abstract class
    Introduce BaseRoleAssignmentPlugin in SecurityRoleManagement namespace, inheriting from PluginBase. This class includes methods for role assignment operations and an abstract method AssignOrRemoveRoles. Added necessary using directives.

---
+++ SecurityRoleManagement/BaseRoleAssignmentPlugin.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xrm.Sdk;
+using SecurityRoleManagement.Services;
+using SecurityRoleManagement.Utilities;
+
+namespace SecurityRoleManagement
+{
+    public abstract class BaseRoleAssignmentPlugin : PluginBase
+    {
+        protected readonly IRoleService _roleService;
+        protected readonly IRoleAssignmentService _roleAssignmentService;
+
+        protected BaseRoleAssignmentPlugin(
+            Type pluginType,
+            IRoleService roleService,
+            IRoleAssignmentService roleAssignmentService
+        )
+            : base(pluginType)
+        {
+            _roleService = roleService;
+            _roleAssignmentService = roleAssignmentService;
+        }
+
+        protected void SetOutputParameters(
+            OutputParameters outputParameters,
+            IPluginExecutionContext context,
+            ITracingService tracer,
+            string wasSuccessfulKey,
+            string errorMessageKey
+        )
+        {
+            PluginUtilities.SetOutputParameters(
+                outputParameters.ToDictionary(wasSuccessfulKey, errorMessageKey),
+                context,
+                tracer
+            );
+        }
+
+        protected EntityCollection RetrieveRoles(
+            IOrganizationService service,
+            string[] roles,
+            ITracingService tracer,
+            out string errorMessage
+        )
+        {
+            return PluginUtilities.RetrieveRoles(
+                _roleService,
+                service,
+                roles,
+                tracer,
+                out errorMessage
+            );
+        }
+
+        protected EntityCollection RetrieveEntities(
+            IOrganizationService service,
+            string[] ids,
+            Func<IOrganizationService, string[], ITracingService, EntityCollection> retrieveMethod,
+            ITracingService tracer
+        )
+        {
+            return PluginUtilities.RetrieveEntities(service, ids, retrieveMethod, tracer);
+        }
+
+        protected List<Guid> GetDistinctEntityIds(
+            EntityCollection entityCollection,
+            string attributeName
+        )
+        {
+            return PluginUtilities.GetDistinctEntityIds(entityCollection, attributeName);
+        }
+
+        protected abstract void AssignOrRemoveRoles(
+            IOrganizationService service,
+            Guid entityId,
+            List<Entity> rolesToAssign,
+            ITracingService tracer,
+            bool willAssign
+        );
+    }
+}
